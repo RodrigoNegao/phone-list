0f5ffec72cd628b03588e0fc450733a07468fca4
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2021-09-07 22:45:47 -0300

    updates

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Grid, Typography } from "@material-ui/core";
 import useStyles from "./style";
 import Todo from "../../components/Todo";
@@ -9,13 +9,18 @@ const DATA = [
   { name: "Model 1" }, //, checked: true
 ];
 
-const taskList = DATA.map((task) => <Todo name={task.name} />);
-
 export default function Home() {
   const classes = useStyles();
 
   const [value, setValue] = useState("");
-  const [dense, setDense] = useState(false);
+  const [tasks, setTask] = useState([]);
+
+  const taskList = tasks.map((task) => <Todo name={task.name} />);
+
+  function addTask(value) {
+    const newTask = { name: value };
+    setTask([...tasks, newTask]);
+  }
 
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -24,68 +29,22 @@ export default function Home() {
 
   function handleKeyPress(e) {
     if (e.key === "Enter") {
-      setValue(value);
+      addTask(value);
       console.log("2", value);
     }
   }
 
   function handleClick() {
-    setValue(value);
+    addTask(value);
     console.log("3", value);
   }
 
+  useEffect(() => {
+    setTask(taskList);
+  }, [value]);
+
   return (
     <Container fixed>
-      <div>
-        <h1>Home</h1>
-        <h2>lista</h2>
-        <h2>hooks</h2>
-      </div>
-      <Grid>
-        <h1>TodoMatic</h1>
-        <form>
-          <h2 className="label-wrapper">
-            <label htmlFor="new-todo-input" className="label__lg">
-              What needs to be done?
-            </label>
-          </h2>
-          <input
-            type="text"
-            id="new-todo-input"
-            className="input input__lg"
-            name="text"
-            autoComplete="off"
-          />
-          <button type="submit" className="btn btn__primary btn__lg">
-            Add
-          </button>
-        </form>
-        <div className="filters btn-group stack-exception">
-          <button type="button" className="btn toggle-btn" aria-pressed="true">
-            <span className="visually-hidden">Show </span>
-            <span>all</span>
-            <span className="visually-hidden"> tasks</span>
-          </button>
-          <button type="button" className="btn toggle-btn" aria-pressed="false">
-            <span className="visually-hidden">Show </span>
-            <span>Active</span>
-            <span className="visually-hidden"> tasks</span>
-          </button>
-          <button type="button" className="btn toggle-btn" aria-pressed="false">
-            <span className="visually-hidden">Show </span>
-            <span>Completed</span>
-            <span className="visually-hidden"> tasks</span>
-          </button>
-        </div>
-        <h2 id="list-heading">3 tasks remaining</h2>
-        <ul
-          role="list"
-          className="todo-list stack-large stack-exception"
-          aria-labelledby="list-heading"
-        >
-          <Todo />
-        </ul>
-      </Grid>
       <Grid>
         <Form
           value={value}
